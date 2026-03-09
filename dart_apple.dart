80b69e4ff5b4d01ef055c9feb9e4aae075fffc96
AppleH9ll <applehell5051@gmail.com>
2026-03-09 21:08:56 +0300

    Add files via upload

--- dart_apple.dart
+++ dart_apple.dart
@@ -1,178 +1,198 @@
-import 'dart:math';
-import 'dart:io';
-
 void main() {
-  print('Добро пожаловать в калькулятор!');
-  print('Доступные операции:');
-  print('1. Арифметические операции (+, -, *, /, ~/, %, pow)');
-  print('2. Операции сравнения (==, !=, >, <, >=, <=)');
-  print('3. Логические операции (&&, ||, !)');
-  print('4. Выход (exit)');
-  
-  while (true) {
-    print('\nВведите операцию (или "exit" для выхода):');
-    String operation = stdin.readLineSync()!.trim().toLowerCase();
-    
-    if (operation == 'exit') {
-      print('Выход из программы...');
+  List<String> students = [
+    'Иванов Иван',
+    'Петров Петр',
+    'Сидорова Анна',
+    'Козлов Дмитрий',
+    'Смирнова Елена'
+  ];
+
+  List<String> subjects = [
+    'Математика',
+    'Физика',
+    'Информатика',
+    'Литература'
+  ];
+
+  List<List<int>> grades = [
+    [5, 4, 5, 3],
+    [3, 3, 2, 4],
+    [5, 5, 5, 5],
+    [4, 4, 4, 4],
+    [2, 3, 3, 3], 
+  ];
+
+  assert(students.length == grades.length);
+  for (var row in grades) {
+    assert(row.length == subjects.length);
+  }
+
+  print('СВОДНАЯ ТАБЛИЦА УСПЕВАЕМОСТИ\n');
+
+  String header = 'Студент'.padRight(20);
+  for (var subject in subjects) {
+    header += subject.padLeft(12);
+  }
+  header += 'Ср.балл'.padLeft(10);
+  print(header);
+
+  List<double> subjectSums = List.filled(subjects.length, 0.0);
+
+  for (int i = 0; i < students.length; i++) {
+    String row = students[i].padRight(20);
+    double studentSum = 0.0;
+
+    for (int j = 0; j < subjects.length; j++) {
+      int grade = grades[i][j];
+      row += grade.toString().padLeft(12);
+      studentSum += grade;
+      subjectSums[j] += grade;
+    }
+
+    double studentAvg = studentSum / subjects.length;
+    row += studentAvg.toStringAsFixed(2).padLeft(10);
+    print(row);
+  }
+
+  String avgRow = 'Средний по предмету'.padRight(20);
+  for (int j = 0; j < subjects.length; j++) {
+    double subjectAvg = subjectSums[j] / students.length;
+    avgRow += subjectAvg.toStringAsFixed(2).padLeft(12);
+  }
+  print(avgRow);
+
+  print('\nПОИСК СТУДЕНТА');
+  String searchName = 'Сидорова Анна';
+  bool found = false;
+  for (int i = 0; i < students.length; i++) {
+    if (students[i] == searchName) {
+      found = true;
+      print('Студент: ${students[i]}');
+      print('Оценки по предметам:');
+      double sum = 0.0;
+      for (int j = 0; j < subjects.length; j++) {
+        print('  ${subjects[j]}: ${grades[i][j]}');
+        sum += grades[i][j];
+      }
+      double avg = sum / subjects.length;
+      String category = _getCategory(avg);
+      print('Средний балл: ${avg.toStringAsFixed(2)}');
+      print('Категория: $category');
       break;
     }
-    
-    switch (operation) {
-      case '+':
-        performArithmetic('+');
-        break;
-      case '-':
-        performArithmetic('-');
-        break;
-      case '*':
-        performArithmetic('*');
-        break;
-      case '/':
-        performArithmetic('/');
-        break;
-      case '~':
-        performArithmetic('~');
-        break;
-      case '%':
-        performArithmetic('%');
-        break;
-      case 'pow':
-        performArithmetic('pow');
-        break;
-      
-      case '==':
-        performComparison('==');
-        break;
-      case '!=':
-        performComparison('!=');
-        break;
-      case '>':
-        performComparison('>');
-        break;
-      case '<':
-        performComparison('<');
-        break;
-      case '>=':
-        performComparison('>=');
-        break;
-      case '<=':
-        performComparison('<=');
-        break;
-      
-      case '&&':
-        performLogical('&&');
-        break;
-      case '||':
-        performLogical('||');
-        break;
-      case '!':
-        performLogical('!');
-        break;
-      
-      default:
-        print('Неизвестная операция. Попробуйте снова.');
+  }
+  if (!found) {
+    print('Студент с именем "$searchName" не найден.');
+  }
+
+  print('\nУНИКАЛЬНЫЕ ОЦЕНКИ');
+  Set<int> uniqueGrades = {};
+  for (var row in grades) {
+    uniqueGrades.addAll(row);
+  }
+  print('Оценки, встречающиеся в журнале: ${uniqueGrades.toList()}');
+
+  print('\nМАКСИМАЛЬНЫЕ И МИНИМАЛЬНЫЕ ОЦЕНКИ ПО ПРЕДМЕТАМ');
+  for (int j = 0; j < subjects.length; j++) {
+    List<int> subjectGrades = [];
+    for (int i = 0; i < students.length; i++) {
+      subjectGrades.add(grades[i][j]);
+    }
+    int maxGrade = subjectGrades.reduce((a, b) => a > b ? a : b);
+    int minGrade = subjectGrades.reduce((a, b) => a < b ? a : b);
+
+    List<String> maxStudents = [];
+    List<String> minStudents = [];
+    for (int i = 0; i < students.length; i++) {
+      if (grades[i][j] == maxGrade) {
+        maxStudents.add(students[i]);
+      }
+      if (grades[i][j] == minGrade) {
+        minStudents.add(students[i]);
+      }
+    }
+
+    print('${subjects[j]}:');
+    print('  Максимальная оценка $maxGrade у студентов: ${maxStudents.join(', ')}');
+    print('  Минимальная оценка $minGrade у студентов: ${minStudents.join(', ')}');
+  }
+
+  print('\nСТУДЕНТЫ С РОВНО ОДНОЙ ДВОЙКОЙ');
+  bool hasSingleTwo = false;
+  for (int i = 0; i < students.length; i++) {
+    int twoCount = 0;
+    int twoIndex = -1;
+    for (int j = 0; j < subjects.length; j++) {
+      if (grades[i][j] == 2) {
+        twoCount++;
+        twoIndex = j;
+      }
+    }
+    if (twoCount == 1) {
+      hasSingleTwo = true;
+      print('${students[i]} — предмет: ${subjects[twoIndex]}');
     }
   }
-}
-
-void performArithmetic(String operation) {
-  try {
-    print('Введите первое число:');
-    double num1 = double.parse(stdin.readLineSync()!);
-    
-    print('Введите второе число:');
-    double num2 = double.parse(stdin.readLineSync()!);
-    
-    double? result;
-    
-    if (operation == '+') {
-      result = num1 + num2;
-    } else if (operation == '-') {
-      result = num1 - num2;
-    } else if (operation == '*') {
-      result = num1 * num2;
-    } else if (operation == '/') {
-      if (num2 == 0) {
-        print('Ошибка: деление на ноль');
-        return;
-      }
-      result = num1 / num2;
-    } else if (operation == '~') {
-      if (num2 == 0) {
-        print('Ошибка: деление на ноль');
-        return;
-      }
-      result = (num1 ~/ num2).toDouble();
-    } else if (operation == '%') {
-      result = num1 % num2;
-    } else if (operation == 'pow') {
-      result = pow(num1, num2).toDouble();
-    }
-    
-    print('Результат: $num1 $operation $num2 = $result');
-  } catch (e) {
-    print('Ошибка ввода. Введите корректные числа.');
+  if (!hasSingleTwo) {
+    print('Нет студентов с ровно одной двойкой.');
   }
-}
 
-void performComparison(String operation) {
-  try {
-    print('Введите первое число:');
-    double num1 = double.parse(stdin.readLineSync()!);
-    
-    print('Введите второе число:');
-    double num2 = double.parse(stdin.readLineSync()!);
-    
-    bool result;
-    
-    if (operation == '==') {
-      result = num1 == num2;
-    } else if (operation == '!=') {
-      result = num1 != num2;
-    } else if (operation == '>') {
-      result = num1 > num2;
-    } else if (operation == '<') {
-      result = num1 < num2;
-    } else if (operation == '>=') {
-      result = num1 >= num2;
+  print('\nПРЕДМЕТЫ СО СРЕДНИМ БАЛЛОМ ВЫШЕ ОБЩЕГО');
+  double totalSum = 0.0;
+  int totalCount = 0;
+  for (var row in grades) {
+    for (var g in row) {
+      totalSum += g;
+      totalCount++;
+    }
+  }
+  double overallAvg = totalSum / totalCount;
+
+  print('Общий средний балл по группе: ${overallAvg.toStringAsFixed(2)}');
+
+  for (int j = 0; j < subjects.length; j++) {
+    double subjectSum = 0.0;
+    for (int i = 0; i < students.length; i++) {
+      subjectSum += grades[i][j];
+    }
+    double subjectAvg = subjectSum / students.length;
+    if (subjectAvg > overallAvg) {
+      print('${subjects[j]} (${subjectAvg.toStringAsFixed(2)})');
+    }
+  }
+
+  print('\nКАТЕГОРИИ УСПЕВАЕМОСТИ');
+  int excellent = 0;
+  int good = 0;
+  int other = 0;
+
+  for (int i = 0; i < students.length; i++) {
+    double sum = 0.0;
+    for (int j = 0; j < subjects.length; j++) {
+      sum += grades[i][j];
+    }
+    double avg = sum / subjects.length;
+    String cat = _getCategory(avg);
+    if (cat == 'отличник') {
+      excellent++;
+    } else if (cat == 'хорошист') {
+      good++;
     } else {
-      result = num1 <= num2;
+      other++;
     }
-    
-    print('Результат: $num1 $operation $num2 = $result');
-  } catch (e) {
-    print('Ошибка ввода. Введите корректные числа.');
   }
+
+  print('Отличники: $excellent');
+  print('Хорошисты: $good');
+  print('Остальные: $other');
 }
 
-void performLogical(String operation) {
-  try {
-    bool value1, value2;
-    
-    if (operation == '!') {
-      print('Введите логическое значение (true/false):');
-      value1 = stdin.readLineSync()!.toLowerCase() == 'true';
-      
-      bool result = !value1;
-      print('Результат: !$value1 = $result');
-    } else {
-      print('Введите первое логическое значение (true/false):');
-      value1 = stdin.readLineSync()!.toLowerCase() == 'true';
-      
-      print('Введите второе логическое значение (true/false):');
-      value2 = stdin.readLineSync()!.toLowerCase() == 'true';
-      
-      bool result;
-      if (operation == '&&') {
-        result = value1 && value2;
-        print('Результат: $value1 && $value2 = $result');
-      } else if (operation == '||') {
-        result = value1 || value2;
-        print('Результат: $value1 || $value2 = $result');
-      }
-    }
-  } catch (e) {
-    print('Ошибка ввода. Введите true или false.');
+String _getCategory(double avg) {
+  if (avg >= 4.5) {
+    return 'отличник';
+  } else if (avg >= 3.5) {
+    return 'хорошист';
+  } else {
+    return 'остальные';
   }
 }
