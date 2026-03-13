d1b7a238d836c90625d2d0b15e0e806724ba4a36
AppleH9ll <applehell5051@gmail.com>
2026-03-13 10:47:44 +0300

    Add files via upload

--- dart_apple.dart
+++ dart_apple.dart
@@ -1,193 +1,88 @@
-void main() {
-  List<String> students = [
-    'Иванов Иван',
-    'Петров Петр',
-    'Сидорова Анна',
-    'Козлов Дмитрий',
-    'Смирнова Елена'
-  ];
-
-  List<String> subjects = [
-    'Математика',
-    'Физика',
-    'Информатика',
-    'Литература'
-  ];
-
-  List<List<int>> grades = [
-    [5, 4, 5, 3],
-    [3, 3, 2, 4],
-    [5, 5, 5, 5],
-    [4, 4, 4, 4],
-    [2, 3, 3, 3], 
-  ];
-
-  print('СВОДНАЯ ТАБЛИЦА УСПЕВАЕМОСТИ\n');
-
-  String header = 'Студент'.padRight(20);
-  for (var subject in subjects) {
-    header += subject.padLeft(12);
+String formatName(String surname, String name, [String? patronymic]) {
+  if (patronymic != null) {
+    return '$surname $name $patronymic';
+  } else {
+    return '$surname $name';
   }
-  header += 'Ср.балл'.padLeft(10);
-  print(header);
-
-  List<double> subjectSums = List.filled(subjects.length, 0.0);
-
-  for (int i = 0; i < students.length; i++) {
-    String row = students[i].padRight(20);
-    double studentSum = 0.0;
-
-    for (int j = 0; j < subjects.length; j++) {
-      int grade = grades[i][j];
-      row += grade.toString().padLeft(12);
-      studentSum += grade;
-      subjectSums[j] += grade;
-    }
-
-    double studentAvg = studentSum / subjects.length;
-    row += studentAvg.toStringAsFixed(2).padLeft(10);
-    print(row);
-  }
-
-  String avgRow = 'Средний по предмету'.padRight(20);
-  for (int j = 0; j < subjects.length; j++) {
-    double subjectAvg = subjectSums[j] / students.length;
-    avgRow += subjectAvg.toStringAsFixed(2).padLeft(12);
-  }
-  print(avgRow);
-
-  print('\nПОИСК СТУДЕНТА');
-  String searchName = 'Сидорова Анна';
-  bool found = false;
-  for (int i = 0; i < students.length; i++) {
-    if (students[i] == searchName) {
-      found = true;
-      print('Студент: ${students[i]}');
-      print('Оценки по предметам:');
-      double sum = 0.0;
-      for (int j = 0; j < subjects.length; j++) {
-        print('  ${subjects[j]}: ${grades[i][j]}');
-        sum += grades[i][j];
-      }
-      double avg = sum / subjects.length;
-      String category = _getCategory(avg);
-      print('Средний балл: ${avg.toStringAsFixed(2)}');
-      print('Категория: $category');
-      break;
-    }
-  }
-  if (!found) {
-    print('Студент с именем "$searchName" не найден.');
-  }
-
-  print('\nУНИКАЛЬНЫЕ ОЦЕНКИ');
-  Set<int> uniqueGrades = {};
-  for (var row in grades) {
-    uniqueGrades.addAll(row);
-  }
-  print('Оценки, встречающиеся в журнале: ${uniqueGrades.toList()}');
-
-  print('\nМАКСИМАЛЬНЫЕ И МИНИМАЛЬНЫЕ ОЦЕНКИ ПО ПРЕДМЕТАМ');
-  for (int j = 0; j < subjects.length; j++) {
-    List<int> subjectGrades = [];
-    for (int i = 0; i < students.length; i++) {
-      subjectGrades.add(grades[i][j]);
-    }
-    int maxGrade = subjectGrades.reduce((a, b) => a > b ? a : b);
-    int minGrade = subjectGrades.reduce((a, b) => a < b ? a : b);
-
-    List<String> maxStudents = [];
-    List<String> minStudents = [];
-    for (int i = 0; i < students.length; i++) {
-      if (grades[i][j] == maxGrade) {
-        maxStudents.add(students[i]);
-      }
-      if (grades[i][j] == minGrade) {
-        minStudents.add(students[i]);
-      }
-    }
-
-    print('${subjects[j]}:');
-    print('  Максимальная оценка $maxGrade у студентов: ${maxStudents.join(', ')}');
-    print('  Минимальная оценка $minGrade у студентов: ${minStudents.join(', ')}');
-  }
-
-  print('\nСТУДЕНТЫ С РОВНО ОДНОЙ ДВОЙКОЙ');
-  bool hasSingleTwo = false;
-  for (int i = 0; i < students.length; i++) {
-    int twoCount = 0;
-    int twoIndex = -1;
-    for (int j = 0; j < subjects.length; j++) {
-      if (grades[i][j] == 2) {
-        twoCount++;
-        twoIndex = j;
-      }
-    }
-    if (twoCount == 1) {
-      hasSingleTwo = true;
-      print('${students[i]} - предмет: ${subjects[twoIndex]}');
-    }
-  }
-  if (!hasSingleTwo) {
-    print('Нет студентов с ровно одной двойкой.');
-  }
-
-  print('\nПРЕДМЕТЫ СО СРЕДНИМ БАЛЛОМ ВЫШЕ ОБЩЕГО');
-  double totalSum = 0.0;
-  int totalCount = 0;
-  for (var row in grades) {
-    for (var g in row) {
-      totalSum += g;
-      totalCount++;
-    }
-  }
-  double overallAvg = totalSum / totalCount;
-
-  print('Общий средний балл по группе: ${overallAvg.toStringAsFixed(2)}');
-
-  for (int j = 0; j < subjects.length; j++) {
-    double subjectSum = 0.0;
-    for (int i = 0; i < students.length; i++) {
-      subjectSum += grades[i][j];
-    }
-    double subjectAvg = subjectSum / students.length;
-    if (subjectAvg > overallAvg) {
-      print('${subjects[j]} (${subjectAvg.toStringAsFixed(2)})');
-    }
-  }
-
-  print('\nКАТЕГОРИИ УСПЕВАЕМОСТИ');
-  int excellent = 0;
-  int good = 0;
-  int other = 0;
-
-  for (int i = 0; i < students.length; i++) {
-    double sum = 0.0;
-    for (int j = 0; j < subjects.length; j++) {
-      sum += grades[i][j];
-    }
-    double avg = sum / subjects.length;
-    String cat = _getCategory(avg);
-    if (cat == 'отличник') {
-      excellent++;
-    } else if (cat == 'хорошист') {
-      good++;
-    } else {
-      other++;
-    }
-  }
-
-  print('Отличники: $excellent');
-  print('Хорошисты: $good');
-  print('Остальные: $other');
 }
 
-String _getCategory(double avg) {
-  if (avg >= 4.5) {
-    return 'отличник';
-  } else if (avg >= 3.5) {
-    return 'хорошист';
-  } else {
-    return 'остальные';
+
+double? calculate(double a, double b, String operation) {
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      if (b == 0) {
+        return null;
+      }
+      return a / b;
+    default:
+      return null;
   }
+}
+
+
+void countSigns(List<int> numbers) {
+  int positive = 0;
+  int negative = 0;
+  int zero = 0;
+
+  for (int n in numbers) {
+    if (n > 0) {
+      positive++;
+    } else if (n < 0) {
+      negative++;
+    } else {
+      zero++;
+    }
+  }
+  print('Положительных: $positive, Отрицательных: $negative, Нулевых: $zero');
+}
+
+
+List<int> transformList(List<int> numbers, int Function(int) transformer) {
+  List<int> result = [];
+  for (int n in numbers) {
+    result.add(transformer(n));
+  }
+  return result;
+}
+
+
+int sumDigits(int n) {
+  if (n < 10) {
+    return n;
+  }
+  return n % 10 + sumDigits(n ~/ 10);
+}
+
+
+void main() {
+  print('Задача 1:');
+  print(formatName('Иванов', 'Иван'));
+  print(formatName('Петров', 'Пётр', 'Сергеевич'));
+
+  print('\nЗадача 2:');
+  print('2 + 3 = ${calculate(2, 3, '+')}');
+  print('5 - 2 = ${calculate(5, 2, '-')}');
+  print('4 * 6 = ${calculate(4, 6, '*')}');
+  print('10 / 2 = ${calculate(10, 2, '/')}');
+  print('10 / 0 = ${calculate(10, 0, '/')}');
+
+  print('\nЗадача 3:');
+  countSigns([-5, 0, 3, -1, 7, 0, 2]);
+
+  print('\nЗадача 4:');
+  List<int> original = [1, 2, 3, 4, 5];
+  List<int> squared = transformList(original, (x) => x * x);
+  print('Квадраты: $squared');
+
+  print('\nЗадача 5:');
+  print('Сумма цифр числа 12345 = ${sumDigits(12345)}');
+  print('Сумма цифр числа 7 = ${sumDigits(7)}');
+  print('Сумма цифр числа 0 = ${sumDigits(0)}');
 }
